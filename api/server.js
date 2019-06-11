e07c1f87ed5e7fc27021d08587584af11dc27851
coriagogo <pkcparis@gmail.com>
2019-06-11 16:23:32 -0400

    mvp goals reached

--- api/server.js
+++ api/server.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const usersRouter = require('../users/users-router.js');
 const authRouter = require('../auth/auth-router.js');
 
 const server = express();
 
+const sessionConfig = {
+  name: 'monkey',
+  secret: 'keep it secret, keep it safe!',
+  resave: false, // if there are no changes to the session don't save it,
+  saveUninitialized: true, // for GDPR compliance
+  cookie: {
+    maxAge: 1000 * 60 * 10, // in milliseconds
+    secure: false, // send cookie only over https, set to true in production
+    httpOnly: true, // always set to true, it means client JS can't access the cookie
+  },
+  store: new KnexSessionStore({
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    knex: require('../database/dbConfig.js'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 30,
+  }),
+}
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.get('/', (req, res) => {
   res.send("I'm working!");
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -6,7 +6,7 @@ const Users = require('../users/users-model.js');
 router.post('/register', (req, res) => {
   let user = req.body;
 
-  const hash = bcrypt.hashSync(user.password, 8);
+  const hash = bcrypt.hashSync(user.password, 10);
 
   user.password = hash;
 
@@ -26,6 +26,7 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -35,5 +36,12 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+router.delete('/', (req, res) => {
+  if (req.session) {
+    req.session.destroy();
+  }
+  res.status(200).json({ message: 'later' });
+})
 
 module.exports = router;
